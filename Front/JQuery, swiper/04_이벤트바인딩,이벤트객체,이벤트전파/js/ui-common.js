beb5baa4db2bbbca2ef587c399ee8d316b5558f2
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2024-04-11 09:57:44 +0900

    04_이벤트바인딩,이벤트객체,이벤트전파

--- Front/JQuery, swiper/04_이벤트바인딩,이벤트객체,이벤트전파/js/ui-common.js
+++ Front/JQuery, swiper/04_이벤트바인딩,이벤트객체,이벤트전파/js/ui-common.js
@@ -42,5 +42,34 @@ $(function () {
   // outerWidth() : 선, 패딩을 포함한 크기 (일반적으로 사용)
   // innerWidth() : 선을 제외한 크기
   // width: 선, 패딩을 제외한 크기
+  console.log($('#header .btn').outerWidth());
+  console.log($('#header .btn').innerWidth());
   console.log($('#header .btn').width());
+
+  // 키보드 이벤트(keydown: 키를 입력한 순간, keyup: 키를 누르고 뗐을 때, 주로 사용)
+  // 제이쿼리 메서드는 get, set 기능을 가지고 있음
+  $('#header .search').on('keyup', function (event) {
+    // setter(): 값을 변경해준다.
+    // $(this).val('안녕하세요');
+
+    // getter(): 값을 가져온다.
+    console.log($(this).val());
+
+    // enter key 확인
+    console.log(event.key);
+    if (event.key === 'Enter') {
+      console.log('push enter');
+    }
+  });
+
+  // 포커스 서치 텍스트 필드로 강제 이동
+  $('header .search').trigger('focus');
+
+  $(document).on('mousemove', function (event) {
+    // clientXY = (뷰포트)브라우저 화면 상에서의 마우스 위치값을 나타내 준다
+    // console.log(event.clientX, event.clientY);
+
+    // html문서에서의 좌표값
+    console.log(event.pageX, event.pageY);
+  });
 });
--- README.md
+++ README.md
@@ -1,13 +1,37 @@
 # AWS-FullStack
 
 1.  html,css3 설정하기
+
 2.  html 시멘틱 마크업 설정
+
 3.  html 태그
+
 4.  css 적용하기
+
 5.  css 선택자
+
 6.  가상 클래스 & 요소 선택자
+
 7.  폰트 설정
+
 8.  텍스트
+
 9.  css초기화
+
 10. margin,padding(마진병합,부모자식,형제요소 수직방향인접시)
+
 11. CJ도너스캠프메인(음수마진,calc함수,min-width)퍼블리싱
+
+12. display특성(block,inline,inline-block,none)
+
+13. display_flex
+
+14. 교원라이프(하단기타메뉴)
+
+15. overflow(마스크,스크롤바)
+
+16. 웹폰트연결(아웃백,본문구글폰트,일부영문,@font-face,woff,eot,ttf포맷변환후연결)
+
+17. 아이콘 폰트(구글 아이콘)
+
+17-1. 아이콘 폰트(awesome)
