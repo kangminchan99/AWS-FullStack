91abb6872e6b9cd03d59766ebcffcdaea80f1500
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2024-04-17 18:03:47 +0900

    4/17

--- Front/JQuery, swiper/10_롯데스윗몰(슬라이더옆에텍스트있는타입,centermode,100퍼슬라이더)/js/ui-common.js
+++ Front/JQuery, swiper/10_롯데스윗몰(슬라이더옆에텍스트있는타입,centermode,100퍼슬라이더)/js/ui-common.js
@@ -3,5 +3,9 @@ $(function () {
     // css로 크기지정시 auto
     slidesPerView: 'auto',
     spaceBetween: 30,
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
   });
 });
