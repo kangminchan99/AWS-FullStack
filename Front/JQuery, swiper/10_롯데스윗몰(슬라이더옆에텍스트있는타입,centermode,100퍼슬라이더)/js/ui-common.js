dc14cc883a0e8724b33f978f6912de1a1cec9581
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2024-04-18 17:38:41 +0900

    4/18일 개인 웹 페이지 프로젝트

--- Front/JQuery, swiper/10_롯데스윗몰(슬라이더옆에텍스트있는타입,centermode,100퍼슬라이더)/js/ui-common.js
+++ Front/JQuery, swiper/10_롯데스윗몰(슬라이더옆에텍스트있는타입,centermode,100퍼슬라이더)/js/ui-common.js
@@ -1,11 +1,30 @@
 $(function () {
+  // 메인 신상
   let mainNew = new Swiper('.main_new .swiper', {
     // css로 크기지정시 auto
     slidesPerView: 'auto',
     spaceBetween: 30,
-    // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
     },
   });
+
+  // 메인 인기상품
+  let mainPopular = new Swiper('.main_popular .swiper', {
+    slidesPerView: 'auto',
+    spaceBetween: 50,
+    // active슬라이드 가운데 배치
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+  // 시작시 슬라이드 이동, slideTo(슬라이드 인덱스, 시간)
+  // 시간이 0일때 자동재생안되는 버그있으므로 1
+  mainPopular.slideTo(3, 1);
 });
