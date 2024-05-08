0c9311d3ffb55ed9c387efd0db7e18f596eb1512
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2024-05-08 12:48:06 +0900

    react 개발환경 설정

---
+++ Front/React/02_컴포넌트 생성,import,export/react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+// import Header from './components/Header ';
+import Header from '@/components/Header';
+
+export default function App() {
+  console.log('rendering done');
+  return (
+    <div>
+      <Header />
+      App
+    </div>
+  );
+}
