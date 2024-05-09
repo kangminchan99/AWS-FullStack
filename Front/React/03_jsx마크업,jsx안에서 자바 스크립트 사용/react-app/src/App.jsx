e518d0d780aba91e864c10ce91df310c03fd24a1
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2024-05-09 10:36:17 +0900

    jsx마크업

--- Front/React/03_jsx마크업,jsx안에서 자바 스크립트 사용/react-app/src/App.jsx
+++ Front/React/03_jsx마크업,jsx안에서 자바 스크립트 사용/react-app/src/App.jsx
@@ -44,27 +44,59 @@ import './App.css';
 //   );
 // }
 
-const pet = {
-  name: '장군이',
+// 한번만 사용하는 데이터는 컴포넌트의 바깥쪽 영역에 배치하여 사용
+// 컴포넌트가 렌더링되면 컴포넌트 함수 안쪽 코드가 재실행되므로
+// const pet = {
+//   name: '장군이',
+//   theme: {
+//     backgroundColor: '#ffc107',
+//     color: '#000',
+//   },
+//   imgUrl: 'http://ossamuiux.com/react-img/dog.jpg',
+// };
+
+// export default function TodoList() {
+//   return (
+//     <div style={pet.theme}>
+//       <h2>{pet.name}&apos;s Todos</h2>
+//       <img className="avatar" src={pet.imgUrl} alt="" />
+//       <ul>
+//         <li>밥먹기</li>
+//         <li>산책하기</li>
+//         <li>까불기</li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+// const baseUrl = 'https://i.imgur.com/';
+
+import { getImageUrl } from './utils';
+
+const person = {
+  name: 'Gregorio Y. Zara',
+  imageId: '7vQD0fP',
+  imageSize: 'b',
   theme: {
-    backgroundColor: '#ffc107',
-    color: '#000',
+    backgroundColor: 'black',
+    color: 'pink',
   },
 };
 
 export default function TodoList() {
   return (
-    <div style={pet.theme}>
-      <h2>{pet.name}&apos;s Todos</h2>
+    <div style={person.theme}>
+      <h1>{person.name}&apos;s Todos</h1>
       <img
         className="avatar"
-        src="http://ossamuiux.com/react-img/dog.jpg"
-        alt=""
+        // src={`${baseUrl}${person.imageId}${person.imageSize}.jpg`}
+        src={getImageUrl(person)}
+        alt={person.name}
       />
       <ul>
-        <li>밥먹기</li>
-        <li>산책하기</li>
-        <li>까불기</li>
+        <li>화상전화 개선</li>
+        <li>항공학 강의 준비</li>
+        <li>알코올 연료 엔진 작업</li>
       </ul>
     </div>
   );
