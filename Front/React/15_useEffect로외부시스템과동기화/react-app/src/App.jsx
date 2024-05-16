8656318374987e8a3ca5b33af885798464822150
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2024-05-16 14:41:22 +0900

    useEffect

--- Front/React/15_useEffect로외부시스템과동기화/react-app/src/App.jsx
+++ Front/React/15_useEffect로외부시스템과동기화/react-app/src/App.jsx
@@ -1,57 +1,18 @@
-import { useState, useRef } from 'react';
-import './App.css';
-import Checkbox from './components/Checkbox';
-import Radio from './components/Radio';
+// Counter.js
+import { useState, useEffect } from 'react';
 
-export default function VideoPlayer() {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const videoRef = useRef(null);
+export default function Counter() {
+  const [count, setCount] = useState(0);
 
-  function handleClick() {
-    setIsPlaying(!isPlaying);
-    if (!isPlaying) {
-      videoRef.current.play();
-    } else {
-      videoRef.current.pause();
+  useEffect(() => {
+    function onTick() {
+      setCount((c) => c + 1);
     }
-  }
 
-  return (
-    <div className="app">
-      <button type="button" onClick={handleClick}>
-        {isPlaying ? 'Pause' : 'Play'}
-      </button>
-      <video
-        ref={videoRef}
-        width="250"
-        src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
-      ></video>
-      <div>
-        <Checkbox checked={true}>
-          <span>동의합니다.</span>
-        </Checkbox>
-        <Checkbox>
-          <span>수락합니다.</span>
-        </Checkbox>
-        <Radio checked={true} group={'selectGroup'}>
-          <span>선택1</span>
-        </Radio>
-        <Radio group={'selectGroup'}>
-          <span>선택2</span>
-        </Radio>
-        <Radio group={'selectGroup'}>
-          <span>선택3</span>
-        </Radio>
-        <Radio checked={true} group={'subject'}>
-          <span>국어</span>
-        </Radio>
-        <Radio group={'subject'}>
-          <span>영어</span>
-        </Radio>
-        <Radio group={'subject'}>
-          <span>수학</span>
-        </Radio>
-      </div>
-    </div>
-  );
+    const id = setInterval(onTick, 1000);
+
+    return () => clearInterval(id);
+  }, []);
+
+  return <h1>{count}</h1>;
 }
